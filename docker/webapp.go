81282d93cd04d1bbe8390df1048b4743a6f10fc7
Colin Campbell <you@example.com>
2018-06-15 16:38:04 +0100

    print football teams from the world cup in json

--- docker/webapp.go
+++ docker/webapp.go
@@ -2,14 +2,67 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+// type WorldCupTeams struct {
+// 	Count int `json:"count"`
+// 	Teams []struct {
+// 		CrestURL         string      `json:"crestUrl"`
+// 		ID               int         `json:"id"`
+// 		Name             string      `json:"name"`
+// 		ShortName        interface{} `json:"shortName"`
+// 		SquadMarketValue interface{} `json:"squadMarketValue"`
+// 	} `json:"teams"`
+// }
+
+// func main() {
+// 	resp, err := http.Get("http://api.football-data.org/v1/competitions/467/teams")
+// 	defer resp.Body.Close()
+// 	if err != nil {
+// 		println(err)
+// 	}
+//
+// 	body, err := ioutil.ReadAll(resp.Body)
+//
+// 	if err != nil {
+// 		println(err)
+// 	}
+//
+// 	var football WorldCupTeams
+// 	err = json.Unmarshal(body, &football)
+// 	if err != nil {
+// 		fmt.Printf("%s", err)
+// 	}
+//
+// 	for _, v := range football {
+// 		fmt.Printf("%s ", v.Teams)
+// 	}
+// }
+
+// func main() {
+func Teams(w http.ResponseWriter, r *http.Request) {
+	url := "http://api.football-data.org/v1/competitions/467/teams"
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	responseString := string(responseData)
+
+	// fmt.Println(responseString)
+	fmt.Fprint(w, responseString)
 }
 
 func main() {
-	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/", Teams)
 	http.ListenAndServe(":8080", nil)
 }
